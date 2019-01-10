ab1b1b741d87450e148b320cf4621b3502b93717
Aaron Helton <mariusagricola@gmail.com>
2019-01-10 12:09:26 -0500

    Update app.py

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, abort, jsonify, Response, url_for
+from requests import get
+from bson.objectid import ObjectId
+from .config import Config
+import boto3, re, time, os, pymongo
+
+# Initialize your application.
+app = Flask(__name__)
+collection = Config.DB.pressReleases
+
+# Define any classes you want to use here, or you could put
+# them in other files and import.
+
+# And start building your routes.
+@app.route('/)
+def index():
+    return(render_template('index.html', data=return_data))
